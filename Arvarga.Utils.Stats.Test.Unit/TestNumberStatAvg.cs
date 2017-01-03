b968215eca832bf1c88813787bddff88c5eb0a1b
adamrotaru <adamrotaru>
2017-01-03 23:44:16 +0100

    Stat: Revive unit tests with .NET Core (Xunit)

--- Arvarga.Utils.Stats.Test.Unit/TestNumberStatAvg.cs
+++ Arvarga.Utils.Stats.Test.Unit/TestNumberStatAvg.cs
@@ -3,18 +3,20 @@
  * (c) 2016 Adam Rotaru
  */
 
-﻿﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace Arvarga.Utils.Stats.Test.Unit
 {
-    [TestClass]
+    //[TestClass]
     public class TestNumberStatAvg
     {
         /// <summary>
         /// Test a simple, normal use case
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestNumberStatAvg_TestSimple()
         {
             NumberStatAvg stat = new NumberStatAvg();
@@ -31,31 +33,33 @@ namespace Arvarga.Utils.Stats.Test.Unit
             stat.Add(5);
             Console.WriteLine($"{stat}");
             // check avg
-            Assert.AreEqual(3, stat.Average, "wrong avg");
-            Assert.AreEqual(5, stat.Count, "wrong count");
-            Assert.AreEqual(1, stat.Min, "wrong min");
-            Assert.AreEqual(5, stat.Max, "wrong max");
+            Assert.Equal<double>(3, stat.Average);
+            Assert.Equal<int>(5, stat.Count);
+            Assert.Equal<double>(1, stat.Min);
+            Assert.Equal<double>(5, stat.Max);
         }
 
         /// <summary>
         /// Test for the no-numbers use case
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestNumberStatAvg_TestZero()
         {
             NumberStatAvg stat = new NumberStatAvg();
             Console.WriteLine($"{stat}");
             // check avg
-            Assert.AreEqual(0, stat.Average, "wrong avg");
-            Assert.AreEqual(0, stat.Count, "wrong count");
-            Assert.AreEqual(0, stat.Min, "wrong min");
-            Assert.AreEqual(0, stat.Max, "wrong max");
+            Assert.Equal<double>(0, stat.Average);
+            Assert.Equal<int>(0, stat.Count);
+            Assert.Equal<double>(0, stat.Min);
+            Assert.Equal<double>(0, stat.Max);
         }
 
         /// <summary>
         /// Test with many numbers (10000)
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestMany()
         {
             NumberStatAvg stat = new NumberStatAvg();
@@ -63,35 +67,36 @@ namespace Arvarga.Utils.Stats.Test.Unit
             for (int i = 0; i < n; ++i) stat.Add(i);
             Console.WriteLine($"{stat}");
             // check avg
-            Assert.AreEqual((double)(n - 1) / (double)2, stat.Average, "wrong avg");
-            Assert.AreEqual(n, stat.Count, "wrong count");
-            Assert.AreEqual(0, stat.Min, "wrong min");
-            Assert.AreEqual(n-1, stat.Max, "wrong max");
+            Assert.Equal<double>((double)(n - 1) / (double)2, stat.Average);
+            Assert.Equal<int>(n, stat.Count);
+            Assert.Equal<double>(0, stat.Min);
+            Assert.Equal<double>(n-1, stat.Max);
         }
 
         /// <summary>
         /// Test median being updated all the time.
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestNumberStatAvg_TestMedianUpdate()
         {
             NumberStatPctileMean stat = new NumberStatPctileMean(100, 0);
             stat.Add(1);
-            Assert.AreEqual(1, stat.Median, "wrong med 1");
+            Assert.Equal<double>(1, stat.Median);
             stat.Add(2);
-            Assert.AreEqual(1, stat.Median, "wrong med 2");
+            Assert.Equal<double>(1, stat.Median);
             stat.Add(3);
-            Assert.AreEqual(2, stat.Median, "wrong med 3");
+            Assert.Equal<double>(2, stat.Median);
             stat.Add(4);
-            Assert.AreEqual(2, stat.Median, "wrong med 4");
+            Assert.Equal<double>(2, stat.Median);
             stat.Add(5);
-            Assert.AreEqual(3, stat.Median, "wrong med 5");
+            Assert.Equal<double>(3, stat.Median);
             stat.Add(6);
-            Assert.AreEqual(3, stat.Median, "wrong med 6");
+            Assert.Equal<double>(3, stat.Median);
             stat.Add(7);
-            Assert.AreEqual(4, stat.Median, "wrong med 7");
-            Assert.AreEqual(7, stat.Count, "wrong count");
-            Assert.AreEqual(7, stat.CountAll, "wrong count all");
+            Assert.Equal<double>(4, stat.Median);
+            Assert.Equal<int>(7, stat.Count);
+            Assert.Equal<long>(7, stat.CountAll);
         }
     }
 }
--- Arvarga.Utils.Stats.Test.Unit/TestNumberStatPctileMean.cs
+++ Arvarga.Utils.Stats.Test.Unit/TestNumberStatPctileMean.cs
@@ -3,18 +3,20 @@
  * (c) 2016 Adam Rotaru
  */
 
-﻿﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace Arvarga.Utils.Stats.Test.Unit
 {
-    [TestClass]
+    //[TestClass]
     public class TestNumberStatPctileMean
     {
         /// <summary>
         /// Test a simple, normal use case
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestNumberStatPctileMean_TestSimple()
         {
             NumberStatPctileMean stat = new NumberStatPctileMean(100, 0);
@@ -31,31 +33,33 @@ namespace Arvarga.Utils.Stats.Test.Unit
             stat.Add(5);
             Console.WriteLine($"{stat}");
             // check avg
-            Assert.AreEqual(3, stat.AverageAll, "wrong avg");
-            Assert.AreEqual(5, stat.CountAll, "wrong count");
-            Assert.AreEqual(1, stat.MinAll, "wrong min");
-            Assert.AreEqual(5, stat.MaxAll, "wrong max");
+            Assert.Equal<double>(3, stat.AverageAll);
+            Assert.Equal<long>(5, stat.CountAll);
+            Assert.Equal<double>(1, stat.MinAll);
+            Assert.Equal<double>(5, stat.MaxAll);
         }
 
         /// <summary>
         /// Test for the no-numbers use case
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestNumberStatPctileMean_TestZero()
         {
             NumberStatPctileMean stat = new NumberStatPctileMean(100, 0);
             Console.WriteLine($"{stat}");
             // check avg
-            Assert.AreEqual(0, stat.AverageAll, "wrong avg");
-            Assert.AreEqual(0, stat.CountAll, "wrong count");
-            Assert.AreEqual(0, stat.MinAll, "wrong min");
-            Assert.AreEqual(0, stat.MaxAll, "wrong max");
+            Assert.Equal<double>(0, stat.AverageAll);
+            Assert.Equal<long>(0, stat.CountAll);
+            Assert.Equal<double>(0, stat.MinAll);
+            Assert.Equal<double>(0, stat.MaxAll);
         }
 
         /// <summary>
         /// Test with many numbers (10000)
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestNumberStatPctileMean_TestMany()
         {
             NumberStatPctileMean stat = new NumberStatPctileMean(100, 0);
@@ -63,17 +67,18 @@ namespace Arvarga.Utils.Stats.Test.Unit
             for (int i = 0; i < n; ++i) stat.Add(i);
             Console.WriteLine($"{stat}");
             // check avg
-            Assert.AreEqual(n - 1 - (double)(100-1)/(double)2, stat.Average, "wrong avg");
-            Assert.AreEqual((double)(n-1)/(double)2, stat.AverageAll, "wrong avg all");
-            Assert.AreEqual(n, stat.CountAll, "wrong count");
-            Assert.AreEqual(0, stat.MinAll, "wrong min all");
-            Assert.AreEqual(n-1, stat.MaxAll, "wrong max all");
+            Assert.Equal<double>(n - 1 - (double)(100-1)/(double)2, stat.Average);
+            Assert.Equal<double>((double)(n-1)/(double)2, stat.AverageAll);
+            Assert.Equal<long>(n, stat.CountAll);
+            Assert.Equal<double>(0, stat.MinAll);
+            Assert.Equal<double>(n-1, stat.MaxAll);
         }
 
         /// <summary>
         /// Test median with an uneven distribution
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestNumberStatPctileMean_TestUnevenDistrib()
         {
             NumberStatPctileMean stat = new NumberStatPctileMean(100, 0);
@@ -83,113 +88,116 @@ namespace Arvarga.Utils.Stats.Test.Unit
             // check avg
             double avg = stat.Average;
             Console.WriteLine($"avg: {avg}");
-            Assert.AreEqual(35, avg, "wrong avg");
+            Assert.Equal<double>(35, avg);
             double med = stat.Median;
             Console.WriteLine($"median: {med}");
-            Assert.AreEqual(25, med, "wrong avg");
-            Assert.AreEqual(n, stat.CountAll, "wrong count");
-            Assert.AreEqual(0, stat.MinAll, "wrong min all");
-            Assert.AreEqual((n-1)*(n-1), stat.MaxAll, "wrong max all");
+            Assert.Equal<double>(25, med);
+            Assert.Equal<long>(n, stat.CountAll);
+            Assert.Equal<double>(0, stat.MinAll);
+            Assert.Equal<double>((n-1)*(n-1), stat.MaxAll);
             // percentiles
-            Assert.IsTrue(Math.Abs(0 - stat.GetPercentile(0)) < 0.000001, "wrong percentile 0");
+            Assert.True(Math.Abs(0 - stat.GetPercentile(0)) < 0.000001, "wrong percentile 0");
 
-            Assert.IsTrue(Math.Abs(0.5 - stat.GetPercentile(0.05)) < 0.000001, "wrong percentile 0.05");
-            Assert.IsTrue(Math.Abs(4 - stat.GetPercentile(0.2)) < 0.000001, "wrong percentile 0.2");
-            Assert.IsTrue(Math.Abs(25 - stat.GetPercentile(0.5)) < 0.000001, "wrong percentile 0.5");
-            Assert.IsTrue(Math.Abs(64 - stat.GetPercentile(0.8)) < 0.000001, "wrong percentile 0.8");
-            Assert.IsTrue(Math.Abs(81 - stat.GetPercentile(0.9)) < 0.000001, "wrong percentile 0.9");
-            Assert.IsTrue(Math.Abs(90.5 - stat.GetPercentile(0.95)) < 0.000001, "wrong percentile 0.95");
-            Assert.IsTrue(Math.Abs(96.2 - stat.GetPercentile(0.98)) < 0.000001, "wrong percentile 0.98");
-            Assert.IsTrue(Math.Abs(98.1 - stat.GetPercentile(0.99)) < 0.000001, "wrong percentile 0.99");
-            Assert.IsTrue(Math.Abs(100 - stat.GetPercentile(1)) < 0.000001, "wrong percentile 1");
+            Assert.True(Math.Abs(0.5 - stat.GetPercentile(0.05)) < 0.000001, "wrong percentile 0.05");
+            Assert.True(Math.Abs(4 - stat.GetPercentile(0.2)) < 0.000001, "wrong percentile 0.2");
+            Assert.True(Math.Abs(25 - stat.GetPercentile(0.5)) < 0.000001, "wrong percentile 0.5");
+            Assert.True(Math.Abs(64 - stat.GetPercentile(0.8)) < 0.000001, "wrong percentile 0.8");
+            Assert.True(Math.Abs(81 - stat.GetPercentile(0.9)) < 0.000001, "wrong percentile 0.9");
+            Assert.True(Math.Abs(90.5 - stat.GetPercentile(0.95)) < 0.000001, "wrong percentile 0.95");
+            Assert.True(Math.Abs(96.2 - stat.GetPercentile(0.98)) < 0.000001, "wrong percentile 0.98");
+            Assert.True(Math.Abs(98.1 - stat.GetPercentile(0.99)) < 0.000001, "wrong percentile 0.99");
+            Assert.True(Math.Abs(100 - stat.GetPercentile(1)) < 0.000001, "wrong percentile 1");
         }
 
         /// <summary>
         /// Test median being updated all the time.
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestNumberStatPctileMean_TestMedianUpdate()
         {
             NumberStatPctileMean stat = new NumberStatPctileMean(100, 0);
             stat.Add(1);
-            Assert.AreEqual(1, stat.Median, "wrong med 1");
+            Assert.Equal<double>(1, stat.Median);
             stat.Add(2);
-            Assert.AreEqual(1, stat.Median, "wrong med 2");
+            Assert.Equal<double>(1, stat.Median);
             stat.Add(3);
-            Assert.AreEqual(2, stat.Median, "wrong med 3");
+            Assert.Equal<double>(2, stat.Median);
             stat.Add(4);
-            Assert.AreEqual(2, stat.Median, "wrong med 4");
+            Assert.Equal<double>(2, stat.Median);
             stat.Add(5);
-            Assert.AreEqual(3, stat.Median, "wrong med 5");
+            Assert.Equal<double>(3, stat.Median);
             stat.Add(6);
-            Assert.AreEqual(3, stat.Median, "wrong med 6");
+            Assert.Equal<double>(3, stat.Median);
             stat.Add(7);
-            Assert.AreEqual(4, stat.Median, "wrong med 7");
-            Assert.AreEqual(7, stat.Count, "wrong count");
-            Assert.AreEqual(7, stat.CountAll, "wrong count all");
+            Assert.Equal<double>(4, stat.Median);
+            Assert.Equal<int>(7, stat.Count);
+            Assert.Equal<long>(7, stat.CountAll);
         }
 
         /// <summary>
         /// Test how too many values are expired
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestNumberStatPctileMean_TestExpireWithCount()
         {
             NumberStatPctileMean stat = new NumberStatPctileMean(2, 0);  // keep only 2
             stat.Add(1);
-            Assert.AreEqual(1, stat.Count, "wrong count 1");
-            Assert.AreEqual(1, stat.Median, "wrong med 1");
+            Assert.Equal<int>(1, stat.Count);
+            Assert.Equal<double>(1, stat.Median);
             stat.Add(2);
-            Assert.AreEqual(2, stat.Count, "wrong count 2");
-            Assert.AreEqual(1, stat.Median, "wrong med 2");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(1, stat.Median);
             stat.Add(3);
-            Assert.AreEqual(2, stat.Count, "wrong count 3");
-            Assert.AreEqual(2, stat.Median, "wrong med 3");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(2, stat.Median);
             stat.Add(4);
-            Assert.AreEqual(2, stat.Count, "wrong count 4");
-            Assert.AreEqual(3, stat.Median, "wrong med 4");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(3, stat.Median);
             stat.Add(5);
-            Assert.AreEqual(2, stat.Count, "wrong count 5");
-            Assert.AreEqual(4, stat.Median, "wrong med 5");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(4, stat.Median);
             stat.Add(6);
-            Assert.AreEqual(2, stat.Count, "wrong count 6");
-            Assert.AreEqual(5, stat.Median, "wrong med 6");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(5, stat.Median);
             stat.Add(7);
-            Assert.AreEqual(2, stat.Count, "wrong count 7");
-            Assert.AreEqual(6, stat.Median, "wrong med 7");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(6, stat.Median);
             Console.WriteLine($"{stat}");
         }
 
         /// <summary>
         /// Test how old values are expired
         /// </summary>
-        [TestMethod]
+        //[TestMethod]
+        [Fact]
         public void TestNumberStatPctileMean_TestExpireWithTime()
         {
             NumberStatPctileMean stat = new NumberStatPctileMean(100, 150);  // keep only for last 210 secs
             DateTime T0 = DateTime.Now.Subtract(new TimeSpan(1, 0, 0));
             stat.SetCreatedTimeTestonly(T0);
             stat.AddTestOnly(1, T0.AddSeconds(100));
-            Assert.AreEqual(1, stat.Count, "wrong count 1");
-            Assert.AreEqual(1, stat.Median, "wrong med 1");
+            Assert.Equal<int>(1, stat.Count);
+            Assert.Equal<double>(1, stat.Median);
             stat.AddTestOnly(2, T0.AddSeconds(200));
-            Assert.AreEqual(2, stat.Count, "wrong count 2");
-            Assert.AreEqual(1, stat.Median, "wrong med 2");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(1, stat.Median);
             stat.AddTestOnly(3, T0.AddSeconds(300));
-            Assert.AreEqual(2, stat.Count, "wrong count 3");
-            Assert.AreEqual(2, stat.Median, "wrong med 3");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(2, stat.Median);
             stat.AddTestOnly(4, T0.AddSeconds(400));
-            Assert.AreEqual(2, stat.Count, "wrong count 4");
-            Assert.AreEqual(3, stat.Median, "wrong med 4");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(3, stat.Median);
             stat.AddTestOnly(5, T0.AddSeconds(500));
-            Assert.AreEqual(2, stat.Count, "wrong count 5");
-            Assert.AreEqual(4, stat.Median, "wrong med 5");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(4, stat.Median);
             stat.AddTestOnly(6, T0.AddSeconds(600));
-            Assert.AreEqual(2, stat.Count, "wrong count 6");
-            Assert.AreEqual(5, stat.Median, "wrong med 6");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(5, stat.Median);
             stat.AddTestOnly(7, T0.AddSeconds(700));
-            Assert.AreEqual(2, stat.Count, "wrong count 7");
-            Assert.AreEqual(6, stat.Median, "wrong med 7");
+            Assert.Equal<int>(2, stat.Count);
+            Assert.Equal<double>(6, stat.Median);
             Console.WriteLine($"{stat}");
         }
     }
